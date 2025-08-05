49cb4407eae7e917af3a083d0572b05724a77d8d
emmotte <emmetttupper1@gmail.com>
2025-08-05 10:58:15 -0700

    feat: Implement dynamic sites

--- app/_sites/[site]/page.tsx
+++ app/_sites/[site]/page.tsx
@@ -1,9 +1,27 @@
 import { notFound } from 'next/navigation';
+import SitePreview from '@/components/site-preview';
+
+const MOCK_DB = {
+  'nook-on-cook': {
+    siteName: 'The Nook on Cook',
+    siteDescription: 'Cook St Village, Victoria, British Columbia',
+  },
+  'another-site': {
+    siteName: 'Another Site',
+    siteDescription: 'Another Description',
+  },
+};
 
 export default function SitePage({ params }: { params: { site: string } }) {
   if (!params.site) {
     notFound();
   }
 
-  return <div>{params.site}</div>;
+  const siteData = MOCK_DB[params.site as keyof typeof MOCK_DB];
+
+  if (!siteData) {
+    notFound();
+  }
+
+  return <SitePreview {...siteData} />;
 }
